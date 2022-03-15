7e97b3a627e9565ecb2afb31b901428a43859bc0
Chris L <clinscott@oxy.edu>
2022-03-15 15:50:24 -0700

    Bot fix

--- lab3_prelab_6.py
+++ lab3_prelab_6.py
@@ -3,17 +3,14 @@
 from pycreate2 import Create2
 import pyhop
 import time
-import random
 
 THRESHOLD_VAL = 30
 
 # Helper methods
-
 def get_state(state):
     state.sees_wall = {
-        'create2': sees_wall(state.agent['create2'])
+        'create2': sees_wall(state.bot['create2'])
     }
-    return state
 
 def sees_wall(bot):
     sensor = bot.get_sensors()
@@ -31,14 +28,13 @@ def sees_wall(bot):
 def drive_forward(bot):
     bot.drive_direct(100, 100)
     time.sleep(.5)
-    bot.drive_stop()
-
+    # bot.drive_stop()
 
 
 def rotate_right(bot):
     bot.drive_direct(-50, 50)
     time.sleep(.5)
-    bot.drive_stop()
+    # bot.drive_stop()
 
 
 # Define operators
@@ -46,16 +42,16 @@ def rotate_right(bot):
 def drive_to_wall(state, a):
     '''Operator: drives a given agent a to the wall.'''
     if not state.sees_wall[a]:
-        print(state.agent[a])
-        drive_forward(state.agent[a]) 
+        drive_forward(state.bot[a]) 
+        get_state(state)
         return state
     return False
 
 def rotate_from_wall(state, a):
     '''Operator: rotates a given agent a away from wall until it cannot see it anymore.'''
     if state.sees_wall[a]:
-        print(state.agent[a])
-        rotate_right(state.agent[a])
+        rotate_right(state.bot[a])
+        get_state(state)
         return state
     return False 
 
@@ -74,43 +70,33 @@ def go_to_wall(state, a):
     '''
     if state.sees_wall[a]:
         return [('rotate_from_wall', a)]
-    return [('drive_to_wall', a)]
+    elif not state.sees_wall[a]:
+        return [('drive_to_wall', a)]
+    return False
 
 pyhop.declare_methods('drive', go_to_wall)
 
 
-# naw_state = pyhop.State('create2') # 'Not at wall' = naw
-# naw_state.sees_wall = { 'create2': True }
-
-
-
-# pyhop.pyhop(naw_state, tasks=[('drive', 'create2')], verbose=3)
-
-
 def main():
     # Initalize bot
-    # port = '/dev/tty.usbserial-DN0266RJ'
-    # baud = {
-    #     'default': 115200,
-    #     'alt': 19200  # shouldn't need this unless you accidentally set it to this
-    # }
-    # bot = Create2(port=port, baud=baud['default'])
-    # bot.start()
-    # bot.full()
-    # Inital bot state with bot sensor data
-    bot = None
+    port = '/dev/tty.usbserial-DN026A6A'
+    baud = {
+        'default': 115200,
+        'alt': 19200  # shouldn't need this unless you accidentally set it to this
+    }
+    bot = Create2(port=port, baud=baud['default'])
+    bot.start()
+    bot.full()
+
+
+    # Inital bot state
     bot_state = pyhop.State('bot_state')
-    bot_state.agent = {'create2': bot }
-    
-    prev_time = time.time()
-    while time.time() - prev_time < 5:
-        bot_state = get_state(bot_state)
-        print(bot_state)
+    bot_state.bot = {'create2': bot }
+    while True:
+        get_state(bot_state)
         # Get and execute plan
-        plan = pyhop.pyhop(bot_state, tasks=[('drive', 'create2')], verbose=3)
+        plan = pyhop.pyhop(bot_state, tasks=[('drive', 'create2')], verbose=1)
         # Execute plan string
-        exec("{method}(bot_state, \"{agent}\")".format(method=plan[0][0], agent=plan[0][1]))
-
-
+        exec("bot_state = {method}(bot_state, \"{agent}\")".format(method=plan[0][0], agent=plan[0][1]))
 if __name__ == '__main__':
     main()
